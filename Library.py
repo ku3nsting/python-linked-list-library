3102882fd23857010920c8c55378ffb5f19e0cd6
ku3nsting <ku3nsting@users.noreply.github.com>
2017-03-29 11:49:32 -0700

    Added menu, execution loop, quit function

--- Library.py
+++ Library.py
@@ -48,11 +48,30 @@ class Library:
             self.numNodes = self.numNodes + 1
             self.currentNode = newNode
 
-    def printLibrary(self):
-            printNode = self.sentinel
+    def removeNode(self, book):
+            searchNode = self.sentinel
             for index in range(self.numNodes):
+                if searchNode.data == book:
+                    searchNode.prev.next = searchNode.next
+                    searchNode.next.prev = searchNode.prev
+                    self.numNodes = self.numNodes - 1
+                    "Book removed from library"
+                    return
+                else:
+                    searchNode = searchNode.next
+            print "Book not found in library"
+
+    def printLibrary(self):
+            print "\n"
+            print "  \tPrinting Library\n"
+            printNode = self.sentinel
+            #iterate through all existing books
+            for index in range(self.numNodes):
+                #Print index and title
                 print str(index + 1) + "\tTitle:\t\t\t" + str(printNode.next.data.title)
+                #Print author name
                 print "\tAuthor:\t\t\t" + str(printNode.next.data.author)
+                #Print publication date
                 print "\tPublication Date:\t" + str(printNode.next.data.pubDate) + "\n"
                 printNode = printNode.next
 
@@ -76,27 +95,133 @@ e = Book("The Giver",
         "Lois Lowry",
         1999,)
 
-#******************************************#
-#INITIALIZE LIBRARY
-#******************************************#
-newLibrary = Library()
-#print "Library Created"
+#***********************************
+# Input validation function:
+# Loops for more input if user-
+# supplied values are invalid.
+#***********************************
+def validate_ints(number):
+    while True:
+        number = input()
+        try:
+            number = int(number)
+        except ValueError:
+            print "Invalid Input. Please try again:\n"
+            continue
+
+        if (0 < number):
+            return number
+        else:
+            print "Invalid Input. Please try again:\n"
+            continue
+
+def testLibrary():
+        #******************************************#
+        #INITIALIZE LIBRARY
+        #******************************************#
+        newLibrary = Library()
+        #print "Library Created"
+
+        #******************************************#
+        #ADD BOOKS
+        #******************************************#
+        newLibrary.addNode(a)
+        newLibrary.addNode(b)
+        newLibrary.addNode(c)
+        newLibrary.addNode(d)
+        newLibrary.addNode(e)
+        #print "books added"
+
+        #******************************************#
+        #PRINT BOOKS
+        #******************************************#
+        newLibrary.printLibrary()
+
+        #******************************************#
+        #ADD BOOKS
+        #******************************************#
+        newLibrary.removeNode(a)
+        newLibrary.removeNode(c)
+        print "books removed"
+
+        #******************************************#
+        #PRINT BOOKS
+        #******************************************#
+        newLibrary.printLibrary()
+
+        #******************************************#
+        #ADD BOOKS
+        #******************************************#
+        newLibrary.addNode(a)
+        newLibrary.addNode(c)
+        print "books added"
+
+        #******************************************#
+        #PRINT BOOKS
+        #******************************************#
+        newLibrary.printLibrary()
+
 
 #******************************************#
-#ADD BOOKS
+#MAIN PROGRAM
 #******************************************#
-newLibrary.addNode(a)
-newLibrary.addNode(b)
-newLibrary.addNode(c)
-newLibrary.addNode(d)
-newLibrary.addNode(e)
-#print "books added"
 
-#******************************************#
-#ADD BOOKS
-#******************************************#
-newLibrary.printLibrary()
+keepGoing = 2
+
+while (keepGoing == 2):
+
+    print "*** Linked List Library Program ***\t"
+
+    print " 1.\t Run automated test"
+    print " 2.\t Run interactive test"
+    print " 3.\t Quit"
+
+    print "\n Input your choice, then press ENTER:\n"
+
+    choice = 0
+    choice = validate_ints(choice)
+
+    #******************************************#
+    #AUTOMATED TEST
+    #******************************************#
+    if choice == 1:
+        print "*** AUTOMATED TEST ***\n"
+        testLibrary()
+
+    #******************************************#
+    #INTERACTIVE TEST
+    #******************************************#
+    if choice == 2:
+        print "*** INTERACTIVE TEST ***"
+        print "\n    Begin interactive test?"
+
+
+    #-----------------------------
+    # ASK USER WHETHER TO CONTINUE
+    #-----------------------------
+    if choice == 3:
+        print "Are you sure you want to quit?"
+        print "1. \tYes"
+        print "2. \tNo\n"
+        print "Input your selection, then press ENTER:\n"
+        keepGoing = validate_ints(keepGoing);
+
+print "Thanks for using Calendar Generator!"
+print "\n"
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+        
 
